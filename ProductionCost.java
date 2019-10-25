4a20b77f4e09902147b50ae0c1aac7785f527d47
Somsock <56707386+Somsock@users.noreply.github.com>
2019-10-25 00:28:56 -0700

    Add files via upload

---
+++ ProductionCost.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+
+public class ProductionCost {
+     public static void main(String[] args){
+         run();
+     }
+     
+     public static void run(){
+         JOptionPane.showMessageDialog(null, "This is a program designed to help you calculate how many book you need to sell to breakeven.");
+         Bundle one = new Bundle();
+         String prompt = "How many books will you order?";
+         one.setQuantity(getValidInt(prompt));
+         prompt = "According to your printer, how much will it cost to produce this many books?";
+         one.setCost(getValidDouble(prompt));
+         prompt = "How much will you be selling the book for?";
+         Book book = new Book(getValidDouble(prompt));
+         one.setBook(book);
+         JOptionPane.showMessageDialog(null, "You will need to sell at least " + one.getBeQuantity() + " books to make back your production costs.");
+         int choice = JOptionPane.showConfirmDialog(null, "Would you like to see all details of this bundle?");
+         if(choice == JOptionPane.YES_OPTION)
+             JOptionPane.showMessageDialog(null, "Here is your bundle information." + one.allString());
+     }
+     //need to account for cancel button
+     private static int getValidInt(String prompt){
+         boolean validInput = false;
+         int count = 0;
+         int num = 0;
+         while(!validInput){
+             try{
+                 num = Integer.parseInt(JOptionPane.showInputDialog(null, prompt));
+                 validInput = true;
+             }catch (NumberFormatException e){
+                 count++;
+                 if(count%5 == 0)
+                 JOptionPane.showMessageDialog(null, "Please enter a valid number.");
+             }
+         }
+         return num;
+     }
+     
+     private static double getValidDouble(String prompt){
+         boolean validInput = false;
+         int count = 0;
+         double num = 0;
+         while(!validInput){
+             try{
+                 num = Double.parseDouble(JOptionPane.showInputDialog(null, prompt));
+                 validInput = true;
+             }catch (NumberFormatException e){
+                 count++;
+                 if(count%5 == 0)
+                 JOptionPane.showMessageDialog(null, "Please enter a valid number.");
+             }
+         }
+         return num;
+     }
+}
