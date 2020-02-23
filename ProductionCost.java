e0447515c812437d24cb7801bd686eba7cd83906
Somsock <56707386+Somsock@users.noreply.github.com>
2020-02-22 18:11:58 -0800

    Update ProductionCost.java

--- ProductionCost.java
+++ ProductionCost.java
@@ -8,27 +8,31 @@ public class ProductionCost {
      public static void run(){
          JOptionPane.showMessageDialog(null, "This is a program designed to help you calculate how many book you need to sell to breakeven.");
          Bundle one = new Bundle();
-         String prompt = "How many books will you order?";
-         one.setQuantity(getValidInt(prompt));
-         prompt = "According to your printer, how much will it cost to produce this many books?";
-         one.setCost(getValidDouble(prompt));
-         prompt = "How much will you be selling the book for?";
-         Book book = new Book(getValidDouble(prompt));
+         one.setQuantity(getValidInt("How many books will you order?"));
+         one.setCost(getValidDouble("According to your printer, how much will it cost to produce this many books?"));
+         Book book = new Book(getValidDouble("How much will you be selling the book for?"));
          one.setBook(book);
          JOptionPane.showMessageDialog(null, "You will need to sell at least " + one.getBeQuantity() + " books to make back your production costs.");
          int choice = JOptionPane.showConfirmDialog(null, "Would you like to see all details of this bundle?");
          if(choice == JOptionPane.YES_OPTION)
              JOptionPane.showMessageDialog(null, "Here is your bundle information." + one.allString());
      }
-     //need to account for cancel button
      private static int getValidInt(String prompt){
          boolean validInput = false;
          int count = 0;
          int num = 0;
+         String input;
          while(!validInput){
              try{
-                 num = Integer.parseInt(JOptionPane.showInputDialog(null, prompt));
-                 validInput = true;
+                 //num = Integer.parseInt(JOptionPane.showInputDialog(null, prompt));
+                 input = JOptionPane.showInputDialog(null, prompt);
+                 if(input.equals(JOptionPane.CANCEL_OPTION)){
+                    break;
+                 }
+                 num = Integer.parseInt(input);
+                 if(num > 0){
+                    validInput = true;
+                 }
              }catch (NumberFormatException e){
                  count++;
                  if(count%5 == 0)
@@ -42,10 +46,18 @@ public class ProductionCost {
          boolean validInput = false;
          int count = 0;
          double num = 0;
+         String input;
          while(!validInput){
              try{
-                 num = Double.parseDouble(JOptionPane.showInputDialog(null, prompt));
-                 validInput = true;
+                 //num = Double.parseDouble(JOptionPane.showInputDialog(null, prompt));
+                 input = JOptionPane.showInputDialog(null, prompt);
+                 if(input.equals(JOptionPane.CANCEL_OPTION)){
+                    break;
+                 }
+                 num = Double.parseDouble(input);
+                 if(num > 0){
+                    validInput = true;
+                 }
              }catch (NumberFormatException e){
                  count++;
                  if(count%5 == 0)
